cf522afc3a3567ec937b14b4d192bdcb44ad7e2e
Stefan Volkmer <s.volkmer@orange-networks.de>
2018-09-04 16:27:20 +0200

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../nodejsluisbot.zip');
+var kuduApi = 'https://nodejsluisbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$nodejsluisbot';
+var password = 'XhhElPQEmlPTTHAhQlsFtFvxHYbPee3Z8D2wodXwdcDTPPioLHwRZdqFQbdR';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('nodejsluisbot publish');
+  } else {
+    console.error('failed to publish nodejsluisbot', err);
+  }
+});
