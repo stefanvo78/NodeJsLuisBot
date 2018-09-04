7d5654532df858931076b716894c8f89702dc656
Stefan Volkmer <s.volkmer@orange-networks.de>
2018-09-04 16:34:52 +0200

    Add Note Intents

--- app.js
+++ app.js
@@ -36,7 +36,13 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 // This default message handler is invoked if the user's utterance doesn't
 // match any intents handled by other dialogs.
 var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send('You reached the default message handler. You said \'%s\'.', session.message.text);
+    session.send("Hi... I'm the note bot sample. I can create new notes, read saved notes to you and delete notes.");
+
+   // If the object for storing notes in session.userData doesn't exist yet, initialize it
+   if (!session.userData.notes) {
+       session.userData.notes = {};
+       console.log("initializing userData.notes in default message handler");
+   }
 });
 
 bot.set('storage', tableStorage);
@@ -80,4 +86,140 @@ bot.dialog('CancelDialog',
 ).triggerAction({
     matches: 'Cancel'
 })
+
+
+// CreateNote dialog
+bot.dialog('CreateNote', [
+    function (session, args, next) {
+        // Resolve and store any Note.Title entity passed from LUIS.
+        var intent = args.intent;
+        var title = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
+
+        var note = session.dialogData.note = {
+          title: title ? title.entity : null,
+        };
+        
+        // Prompt for title
+        if (!note.title) {
+            builder.Prompts.text(session, 'What would you like to call your note?');
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        var note = session.dialogData.note;
+        if (results.response) {
+            note.title = results.response;
+        }
+
+        // Prompt for the text of the note
+        if (!note.text) {
+            builder.Prompts.text(session, 'What would you like to say in your note?');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        var note = session.dialogData.note;
+        if (results.response) {
+            note.text = results.response;
+        }
+        
+        // If the object for storing notes in session.userData doesn't exist yet, initialize it
+        if (!session.userData.notes) {
+            session.userData.notes = {};
+            console.log("initializing session.userData.notes in CreateNote dialog");
+        }
+        // Save notes in the notes object
+        session.userData.notes[note.title] = note;
+
+        // Send confirmation to user
+        session.endDialog('Creating note named "%s" with text "%s"',
+            note.title, note.text);
+    }
+]).triggerAction({ 
+    matches: 'Note.Create',
+    confirmPrompt: "This will cancel the creation of the note you started. Are you sure?" 
+}).cancelAction('cancelCreateNote', "Note canceled.", {
+    matches: /^(cancel|nevermind)/i,
+    confirmPrompt: "Are you sure?"
+});
+
+
+// Delete note dialog
+bot.dialog('DeleteNote', [
+    function (session, args, next) {
+        if (noteCount(session.userData.notes) > 0) {
+            // Resolve and store any Note.Title entity passed from LUIS.
+            var title;
+            var intent = args.intent;
+            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
+            if (entity) {
+                // Verify that the title is in our set of notes.
+                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
+            }
+            
+            // Prompt for note name
+            if (!title) {
+                builder.Prompts.choice(session, 'Which note would you like to delete?', session.userData.notes);
+            } else {
+                next({ response: title });
+            }
+        } else {
+            session.endDialog("No notes to delete.");
+        }
+    },
+    function (session, results) {
+        delete session.userData.notes[results.response.entity];        
+        session.endDialog("Deleted the '%s' note.", results.response.entity);
+    }
+]).triggerAction({
+    matches: 'Note.Delete'
+}).cancelAction('cancelDeleteNote', "Ok - canceled note deletion.", {
+    matches: /^(cancel|nevermind)/i
+});
+
+// Helper function to count the number of notes stored in session.userData.notes
+function noteCount(notes) {
+
+    var i = 0;
+    for (var name in notes) {
+        i++;
+    }
+    return i;
+}
+
+// Read note dialog
+bot.dialog('ReadNote', [
+    function (session, args, next) {
+        if (noteCount(session.userData.notes) > 0) {
+           
+            // Resolve and store any Note.Title entity passed from LUIS.
+            var title;
+            var intent = args.intent;
+            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
+            if (entity) {
+                // Verify it's in our set of notes.
+                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
+            }
+            
+            // Prompt for note name
+            if (!title) {
+                builder.Prompts.choice(session, 'Which note would you like to read?', session.userData.notes);
+            } else {
+                next({ response: title });
+            }
+        } else {
+            session.endDialog("No notes to read.");
+        }
+    },
+    function (session, results) {        
+        session.endDialog("Here's the '%s' note: '%s'.", results.response.entity, session.userData.notes[results.response.entity].text);
+    }
+]).triggerAction({
+    matches: 'Note.ReadAloud'
+}).cancelAction('cancelReadNote', "Ok.", {
+    matches: /^(cancel|nevermind)/i
+});
+
 
